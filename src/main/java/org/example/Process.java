c191365c0d504830dfb86b3d54cf488307cdfb95
Mohammed Zeinhom <mohamedzenhom200@gmail.com>
2024-11-30 21:55:38 +0200

    Process Class added

---
+++ src/main/java/org/example/Process.java
@@ -0,0 +1,79 @@
+package org.example;
+
+public class Process implements Comparable<Process> {
+    private final String name;
+    private final int arrivalTime;
+    private final int burstTime;
+    private final int priority;
+    private int remainingBurstTime;
+    private int completionTime;
+    private int turnAroundTime;
+    private int waitingTime;
+
+    public Process(String name, int arrivalTime, int burstTime, int priority) {
+        this.name = name;
+        this.arrivalTime = arrivalTime;
+        this.burstTime = burstTime;
+        this.priority = priority;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getArrivalTime() {
+        return arrivalTime;
+    }
+
+    public int getBurstTime() {
+        return burstTime;
+    }
+
+    public int getPriority() {
+        return priority;
+    }
+
+    public int getRemainingBurstTime() {
+        return remainingBurstTime;
+    }
+
+    public int getCompletionTime() {
+        return completionTime;
+    }
+
+    public int getTurnAroundTime() {
+        return turnAroundTime;
+    }
+
+    public int getWaitingTime() {
+        return waitingTime;
+    }
+    public void setRemainingBurstTime(int remainingBurstTime) {
+        this.remainingBurstTime = remainingBurstTime;
+    }
+    public void setCompletionTime(int completionTime) {
+        this.completionTime = completionTime;
+    }
+    public void setTurnAroundTime(int turnAroundTime) {
+        this.turnAroundTime = turnAroundTime;
+    }
+    public void setWaitingTime(int waitingTime) {
+        this.waitingTime = waitingTime;
+    }
+
+    @Override
+    public int compareTo(Process other) {
+        return Integer.compare(this.burstTime, other.burstTime);
+    }
+
+
+    @Override
+    public String toString() {
+        return "Process{" +
+                "name='" + name + '\'' +
+                ", arrivalTime=" + arrivalTime +
+                ", burstTime=" + burstTime +
+                ", priority=" + priority +
+                '}';
+    }
+}
